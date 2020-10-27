1ab40b0d481d4323a33456e43242d63046e63e5c
LemonIT.CN <liuri@lemonit.cn>
2020-10-27 16:26:21 +0800

    完成了UtilsProcessAnswerExcel模块

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 
 from model.question_data import QuestionData
 from service.presolver import PResolver
+from service.utils_process_answer_excel import UtilsProcessAnswerExcel
+
 
 # question_list = PResolver.read_excel('/Users/liuri/Documents/code/lemonit-workdata/易学而乐/2020年3月开始e学数独第一期资料/九宫题目模板.xlsx')
 # excel_path = '/Users/liuri/Desktop/易学数独算法/窗口数独20200613.xlsx'
@@ -9,22 +11,46 @@ from service.presolver import PResolver
 # excel_path = '/Users/liuri/Desktop/易学数独算法/斜线数独20200613.xlsx'
 
 # 正常情况下，求解时使用
-if len(sys.argv) < 2:
-    print('请您传入要处理的Excel的路径')
-    sys.exit(1)
-excel_path = sys.argv[1]
-question_list = PResolver.read_excel(excel_path)
+def action_solve():
+    if len(sys.argv) < 3:
+        print('请您传入要处理的Excel的路径')
+        sys.exit(1)
+    excel_path = sys.argv[2]
+    question_list = PResolver.read_excel(excel_path)
+
+    def calculate_success_callback(question: QuestionData):
+        PResolver.write_calculate_result_data_to_excel(excel_path, [question])
+        rule_excel_path_split = excel_path.split('/')
+        rule_excel_path_split.pop(len(rule_excel_path_split) - 1)
+        PResolver.write_check_rule_data_to_excel(
+            '/'.join(rule_excel_path_split) + '/check-rule-' + question.question_key + '.xlsx', [question])
+
+    PResolver.calculate_answer_list(question_list, calculate_success_callback)
 
 
-def calculate_success_callback(question: QuestionData):
-    PResolver.write_calculate_result_data_to_excel(excel_path, [question])
-    rule_excel_path_split = excel_path.split('/')
-    rule_excel_path_split.pop(len(rule_excel_path_split) - 1)
-    PResolver.write_check_rule_data_to_excel(
-        '/'.join(rule_excel_path_split) + '/check-rule-' + question.question_key + '.xlsx', [question])
+# 处理答案excel，将正常带有答案的excel转换为答案独自占用一行的格式，方便直接导入图片生成工具
+def action_process_answer_excel():
+    if len(sys.argv) < 4:
+        print('请传入合法的参数  process_answer_excel excel路径 DBN参数')
+        sys.exit(1)
+    UtilsProcessAnswerExcel.process(sys.argv[2], sys.argv[3])
 
 
-PResolver.calculate_answer_list(question_list, calculate_success_callback)
+def main():
+    if len(sys.argv) < 2:
+        print('参数有误')
+        sys.exit(1)
+    action = sys.argv[1]
+    if action == 'solve':
+        action_solve()
+    elif action == 'process_answer_excel':
+        action_process_answer_excel()
+    else:
+        print('无法识别的Action')
+
+
+if __name__ == '__main__':
+    main()
 
 # 生成数感练习题目时使用
 # question_seed = QuestionData()
---
+++ service/utils_process_answer_excel.py
@@ -0,0 +1,54 @@
+import shutil
+
+import openpyxl
+
+
+class UtilsProcessAnswerExcel:
+    @staticmethod
+    def process(excel_path: str, answer_dbn_params: str):
+        print('开始读取Excel中的题目数据...')
+        excel_path_new = excel_path.replace('.xlsx', '_ans.xlsx')
+        shutil.copyfile(excel_path, excel_path_new)
+        workbook = openpyxl.load_workbook(excel_path_new)
+        row_index = 0
+        # 题编号 : 题答案
+        question_data_pool = {}
+        # 题目号 : 答案行索引
+        question_location_pool = {}
+        question_list = []
+        process_question_num = ''
+        row_index = 0
+        for row in workbook.worksheets[0].rows:
+            row_index = row_index + 1
+            question_num = row[3].value
+            if question_num not in question_num:
+                question_data_pool[question_num] = row[9]
+            if process_question_num != question_num:
+                # 题号改变了
+                if process_question_num == '':
+                    # 是第一道题
+                    process_question_num = question_num
+                    print('首题：' + str(question_num))
+                else:
+                    # 不是第一道题，写入上一道题的数据
+                    process_question_num = question_num
+                    question_list.append(question_num)
+                    row_index = row_index + 1
+                    question_location_pool[question_num] = row_index
+                    # row_index = row_index + 1
+                    print('非首题：' + str(question_num))
+        for question_num in question_list:
+            new_row_index = question_location_pool[question_num]
+            workbook.worksheets[0].insert_rows(new_row_index)
+
+            workbook.worksheets[0].cell(new_row_index, 1, workbook.worksheets[0].cell(new_row_index - 1, 1).value)
+            workbook.worksheets[0].cell(new_row_index, 2, workbook.worksheets[0].cell(new_row_index - 1, 2).value)
+            workbook.worksheets[0].cell(new_row_index, 3, workbook.worksheets[0].cell(new_row_index - 1, 3).value)
+            workbook.worksheets[0].cell(new_row_index, 4, workbook.worksheets[0].cell(new_row_index - 1, 4).value)
+            workbook.worksheets[0].cell(new_row_index, 5, workbook.worksheets[0].cell(new_row_index - 1, 5).value)
+            workbook.worksheets[0].cell(new_row_index, 6, 'DBN')
+            workbook.worksheets[0].cell(new_row_index, 7, workbook.worksheets[0].cell(new_row_index - 1, 10).value)
+            workbook.worksheets[0].cell(new_row_index, 8, answer_dbn_params)
+            workbook.worksheets[0].cell(new_row_index, 9, workbook.worksheets[0].cell(new_row_index - 1, 9).value)
+            print('完善数据：' + question_num)
+        workbook.save(excel_path_new)
